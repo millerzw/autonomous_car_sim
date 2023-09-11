f53522e7fc8fbb81b24750be7a0a08e367687f7b
Zach Miller <zchatk25966@gmail.com>
2023-09-11 14:15:54 -0700

    Canvas can now loop and display car's updated position
    Updated the car canvas relationship to update car positions correctly. Controller not working, will need to use different method to capture user inputs, likely directly from the window instead of the keyboard directly

--- car.py
+++ car.py
@@ -2,14 +2,28 @@ from tkinter import *
 class Car():
     
     
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, canvas):
         self.x = x
         self.y = y
         self.width=width
         self.height=height
+        self.canvas=canvas
+        
+        self.controller = Controller()
         
     
-    def draw(self, canvas):
-        canvas.create_rectangle(20,20,40,40, fill='white', tag='car')
+    def draw(self):
+        self.canvas.delete('car')
+        self.canvas.create_rectangle(self.x-self.width/2,self.y-self.height/2,self.x+self.width/2,self.y+self.height/2, 
+                                fill='white', 
+                                tag='car')
+        
+    def update(self):
+        #self.canvas.delete('car')
+        self.draw()
+        if (self.controller.forward):
+            self.y-=2
+            
+        window.after(2,self.update)
         
     
